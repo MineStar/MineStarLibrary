161fb3d287d7922034aec70bf51fcacb5b1e6f73
Meldanor <Bhaals@gmx.de>
2012-05-15 23:19:44 +0200

    Add interface Statistic and StatisticType for the Illuminati project Add entry as a wrapper for two objects
    Signed-off-by: Meldanor <Bhaals@gmx.de>

---
+++ src/main/java/de/minestar/minestarlibrary/stats/Statistic.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2012 MineStar.de 
+ * 
+ * This file is part of MineStarLibrary.
+ * 
+ * MineStarLibrary is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * MineStarLibrary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with MineStarLibrary.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.minestar.minestarlibrary.stats;
+
+import java.util.List;
+import java.util.Map.Entry;
+
+/**
+ * An interface for statistic to persistate. The server has to run Illuminati
+ * and call the function registerStatistic() to register statistics. <br>
+ * IMPORTANT: This class must provide an empty constructor!
+ * 
+ */
+public interface Statistic {
+
+    /** @return The name of the plugin the statistic is called from */
+    public String getPluginName();
+
+    /**
+     * @return The name of the statistic. This is used to identify what the
+     *         statistic is about
+     */
+    public String getName();
+
+    /**
+     * @return An array of Strings which provides information about the head. <br>
+     *         Must be in this format: <br>
+     *         <code>AttributeName:StatisticType.getName()</code>
+     * 
+     * @see StatisticType StatisticType
+     * */
+    public Entry<String, StatisticType>[] getHead();
+
+    /**
+     * @return An array of objects. These are the data of the statistic. They
+     *         must fit with the datatypes give in {@link Statistic.getHead}
+     */
+    public List<Object> getData();
+
+}
---
+++ src/main/java/de/minestar/minestarlibrary/stats/StatisticType.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2012 MineStar.de 
+ * 
+ * This file is part of Illuminati.
+ * 
+ * Illuminati is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * Illuminati is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Illuminati.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.minestar.minestarlibrary.stats;
+
+import java.util.NoSuchElementException;
+
+public enum StatisticType {
+
+    // @formatter:off
+    INT     (Integer.class,     "INT"),
+    FLOAT   (Float.class,       "FLOAT"),
+    DOUBLE  (Double.class,      "DOUBLE"),
+    LONG    (Long.class,        "BIGINT"),
+    STRING  (String.class,      "TEXT"),
+    CHAR    (Character.class,   "CHAR"),
+    DATE    (String.class,      "DATE"),
+    TIME    (String.class,      "TIME"),
+    DATETIME(String.class,      "DATETIME");
+    // @formatter:on
+
+    /** The class representing the data in Java */
+    private final Class<?> clazz;
+    /** The type name which is used in a SQL database */
+    private final String databaseTypeName;
+
+    private StatisticType(Class<?> clazz, String databaseDatatype) {
+        this.clazz = clazz;
+        this.databaseTypeName = databaseDatatype;
+    }
+
+    /** @return The type name which is used in a SQL database for this datatype */
+    public String getName() {
+        return databaseTypeName;
+    }
+
+    /** @return The class representing the data in Java */
+    public Class<?> getClazz() {
+        return clazz;
+    }
+
+    /**
+     * Getting the StatisticType which has the
+     * 
+     * @param typeName
+     * @return
+     */
+    public static StatisticType findType(String typeName) {
+        for (StatisticType type : values()) {
+            if (type.getName().equals(typeName))
+                return type;
+        }
+        throw new NoSuchElementException("Can't find a StatisticType for name '" + typeName + "'");
+    }
+}
